4cc818552f525600fdaf64bc9b4300bd8e4c7c96
houyh <houge1422841169@qq.com>
2018-08-23 16:07:53 +0800

    add readme

--- test2.go
+++ test2.go
@@ -1,17 +1,21 @@
 package main
 import "fmt"
 func main(){
+	//go语言切片练习
 	s:= make([]string,3)
 	fmt.Println(s)
 	fmt.Println(len(s))
+	//set方法
 	s[0] = "a"
 	s[1] = "b"
 	s[2] = "c"
 	fmt.Println(s)
+	//append方法
 	s = append(s,"d")
 	fmt.Println(s)
 	c := make([]string,len(s))
 	fmt.Println(c)
+	//复制切片
 	copy(c,s)
 	fmt.Println(c)
 	l := s[2:5]
@@ -21,6 +25,7 @@ func main(){
 	fmt.Println(k)
 	fmt.Println(m)
 	///////////////////////
+	//数组求和range用法
 	nums := []int{1,2,3}
 	sum := 0
 	for _,num := range nums{
@@ -29,3 +34,27 @@ func main(){
 	fmt.Println(sum)
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
